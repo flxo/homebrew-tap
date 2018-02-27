9c69a257a7d38f2b03951c22722a843296806e2b
Felix Obenhuber <felix@obenhuber.de>
2018-02-27 14:53:07 +0100

    Fix sha256 of rogcat v0.2.15 release

--- rogcat.rb
+++ rogcat.rb
@@ -2,7 +2,7 @@ class Rogcat < Formula
   desc "adb logcat wrapper and log processor"
   homepage "https://github.com/flxo/rogcat"
   url "https://github.com/flxo/rogcat/releases/download/v0.2.15/rogcat-v0.2.15-x86_64-apple-darwin.tar.gz"
-  sha256 "40bb1e0e4051061b5f8b43353f733833d6f254ac"
+  sha256 "fbc1f4c5c8a006b9bb7a9b5c12c2048cc2c2090053a4ca7c27c58d0d669c1612"
 
   bottle :unneeded
 
