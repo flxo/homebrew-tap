2064cac6cf6ef3b9e9ea704ee3412a514f52ab96
Felix Obenhuber <felix.obenhuber@esrlabs.com>
2018-10-06 18:25:54 +0200

    Update rogcat to v0.2.17

--- rogcat.rb
+++ rogcat.rb
@@ -1,9 +1,8 @@
 class Rogcat < Formula
   desc "adb logcat wrapper and log processor"
   homepage "https://github.com/flxo/rogcat"
-  url "https://github.com/flxo/rogcat/releases/download/v0.2.16/rogcat-v0.2.16-x86_64-apple-darwin.tar.gz"
-  sha256 "baea5b65749591b0c472d8f9cf814c06a9fcc74f807c8e2b264c1d0ee52b4650"
-
+  url "https://github.com/flxo/rogcat/releases/download/v0.2.17/rogcat-v0.2.17-x86_64-apple-darwin.tar.gz"
+  sha256 "c51361983b29157b28ecfe05eb5e94da182bc11aa31ee8ff57a86d035fac6d40"
   bottle :unneeded
 
   def install
