c51cbee675df8bb217d24993773562c8ca2627c3
Jia Chen <fashioncj@users.noreply.github.com>
2020-10-19 15:41:15 +0800

    Update Rogcat to 0.4.1
    Update Rogcat to 0.4.1

--- rogcat.rb
+++ rogcat.rb
@@ -1,8 +1,8 @@
 class Rogcat < Formula
   desc "adb logcat wrapper and log processor"
   homepage "https://github.com/flxo/rogcat"
-  url "https://github.com/flxo/rogcat/releases/download/v0.4.0/rogcat-v0.4.0-x86_64-apple-darwin.tar.gz"
-  sha256 "e3125e8598e445134bb02a113ffd5b675da1297474e672268f012eefbf4a126c"
+  url "https://github.com/flxo/rogcat/releases/download/v0.4.1/rogcat-v0.4.1-x86_64-apple-darwin.tar.gz"
+  sha256 "17b2740f2c3e2c599c500f36e8a13d5c1cf9879fdd814574f9dd20fcdbfb2832"
   bottle :unneeded
 
   def install
